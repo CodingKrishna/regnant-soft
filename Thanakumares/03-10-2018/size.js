44db0684661773f9b90d88ea417ee6ea29e84797
kumares93 <kumares93@gmail.com>
2018-10-03 19:44:21 +0530

    Updated size.js

--- Thanakumares/03-10-2018/size.js
+++ Thanakumares/03-10-2018/size.js
@@ -1,7 +1,10 @@
-var size=["xl","xxxl","l","m","s","l","xxl","s","m"];
-size.sort(//function (a,b) {
-    //  if(a=="s"&&b=="m"||a=="m"&&b=="l"||a=="l"&&b=="xl") return false;
-    // else if(a=="xl"&&b=="xxl"||a=="xxl"&&b=="xxxl") return false;
-    // else return true;}    
-    );
-console.log(size);
+
+module.exports= function sorting(array) {
+ array.sort(function (a,b) {
+    if(a=="s"&&b=="m"||a=="m"&&b=="l"||a=="l"&&b=="xl") return false;
+    else if(a=="xl"&&b=="xxl"||a=="xxl"&&b=="xxxl"||a=="xl"&&b=="xxxl") return false;
+    else return true;
+}    
+);
+return array;
+}
---
+++ Thanakumares/03-10-2018/size_test.js
@@ -0,0 +1,5 @@
+var sorting=require("./size");
+var size=["xl","xxxl","l","m","s","l","xxl","s","m"];
+console.log(size);
+var newArr=sorting(size);
+console.log(newArr);
