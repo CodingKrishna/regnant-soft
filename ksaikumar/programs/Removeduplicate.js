f12005fa5c657fadc134ad763f3012025f769333
kumarsaik <41636389+kumarsaik@users.noreply.github.com>
2018-11-10 19:27:20 +0530

    Update Removeduplicate.js

--- ksaikumar/programs/Removeduplicate.js
+++ ksaikumar/programs/Removeduplicate.js
@@ -5,8 +5,6 @@ for(let i=0;i<arr.length;i++){
         if(arr[i]==arr[j]){
            arr.splice(j,1); 
          }             
-        // else {
-        // newarr.push(arr[i]);}
     } 
 }
     console.log(arr);
